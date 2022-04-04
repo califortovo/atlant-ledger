4d1809abc1cf6f6db5d337cbdfbd0d86c9879e37
Alexandr Spiridonov <califortovo@gmail.com>
2022-04-04 19:17:59 +0300

    init commit

---
+++ src/test/java/com/atlant/ledger/MainControllerTest.java
@@ -0,0 +1,33 @@
+package com.atlant.ledger;
+
+import com.atlant.ledger.controller.MainController;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.hamcrest.Matchers.containsString;
+import static
+        org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static
+        org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static
+        org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static
+        org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+
+
+@WebMvcTest(MainController.class)
+public class MainControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void testMainController() throws Exception {
+        mockMvc.perform(get("/"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("index"))
+                .andExpect(content().string(containsString("Welcome to")));
+    }
+}
